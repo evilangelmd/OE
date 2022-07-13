c77537387614bea2304ef0aaee04fbd15125bd7c
evilangelmd <maxim.rm90@gmail.com>
2022-07-13 09:19:09 +0200

    added storage file

---
+++ storage.js
@@ -0,0 +1,55 @@
+(function(){
+let storage = {
+        get: function(key){
+            if (!this.isSupported()) {
+                return false;
+            } else {
+                try {
+                    const _data = win.localStorage.getItem(key);
+                    if (_data !== null) {
+                        const _json = JSON.parse(_data);
+                        return _json.json;
+                    }
+                } catch (e) {}
+                return false;
+            }
+        },
+        set: function(key, value){
+            if (!this.isSupported()) {
+                return false;
+            } else {
+                win.localStorage.setItem(key, JSON.stringify(
+                    {
+                        'json': value
+                    }
+                ));
+                return true;
+            }
+        },
+        remove: function(key) {
+            if (!this.isSupported()) {
+                return false;
+            } else {
+                win.localStorage.removeItem(key);
+                return true;
+            }
+        },
+        isSupported: function() {
+            try {
+                win.localStorage.setItem('test', '1');
+                win.localStorage.removeItem('test');
+                return true;
+            } catch (e) {
+                console.log('errore sulla storage');
+                console.log(e);
+                return false;
+            }
+        }
+    };
+  
+   if(typeof OE !== 'object') {
+        win.OE = {};
+    }
+    
+    win.OE.storage = storage;
+})()
